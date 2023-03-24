834fe711edf9c824c658b37020888b19b48f9187
Mirian Santana <miriansantana93@gmail.com>
2023-03-24 20:20:27 -0300

    Added tableView on class TableViewPizzaViewController

---
+++ ProjectPizzaClosureThree/ProjectPizzaClosureThree/TableViewPizzaViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewPizzaViewController.swift
+//  ProjectPizzaClosureThree
+//
+//  Created by Mirian Santana on 24/03/23.
+//
+
+import UIKit
+
+class TableViewPizzaViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
